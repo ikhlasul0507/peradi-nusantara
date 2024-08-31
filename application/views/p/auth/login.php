543c7c0037402a6d09a8a4add7dda96e2ec04b56
Ikhlasul Amal <54785909+ikhlasul0507@users.noreply.github.com>
2024-08-31 14:07:52 +0700

    change pass db

--- application/views/p/auth/login.php
+++ application/views/p/auth/login.php
@@ -21,7 +21,7 @@
     <link href="<?= base_url('assets/p/sistem/');?>css/sb-admin-2.min.css" rel="stylesheet">
     <style type="text/css">
         .bg-register-image {
-            background: url("https://kompassindonesianews.com/wp-content/uploads/2024/05/IMG-20240505-WA0232.jpg");
+            background: url("<?= base_url('assets/p/sistem/img/gambarlogin.png');?>");
             background-position: center;
             background-size: cover;
         }
@@ -36,9 +36,9 @@
         <div class="card o-hidden border-0 shadow-lg my-5">
             <div class="card-body p-0">
                 <!-- Nested Row within Card Body -->
-                <div class="row" style="height: 576px">
-                    <div class="col-lg-5 d-none d-lg-block bg-register-image"></div>
-                    <div class="col-lg-7">
+                <div class="row" style="height: 666px">
+                    <div class="col-lg-6 d-none d-lg-block bg-register-image"></div>
+                    <div class="col-lg-6">
                         <div class="p-5">
                             <div class="text-center">
                                 <h1 class="h4 text-gray-900 mb-4">Masuk Sistem</h1>
--- application/views/p/auth/register.php
+++ application/views/p/auth/register.php
@@ -21,7 +21,7 @@
     <link href="<?= base_url('assets/p/sistem/');?>css/sb-admin-2.min.css" rel="stylesheet">
     <style type="text/css">
         .bg-register-image {
-            background: url("assets/p/sistem/img/gambarlogin.png");
+            background: url("<?= base_url('assets/p/sistem/img/gambarlogin.png');?>");
             background-position: center;
             background-size: cover;
         }
@@ -37,8 +37,8 @@
             <div class="card-body p-0">
                 <!-- Nested Row within Card Body -->
                 <div class="row">
-                    <div class="col-lg-5 d-none d-lg-block bg-register-image"></div>
-                    <div class="col-lg-7">
+                    <div class="col-lg-6 d-none d-lg-block bg-register-image"></div>
+                    <div class="col-lg-6">
                         <div class="p-5">
                             <div class="text-center">
                                 <h1 class="h4 text-gray-900 mb-4">Formulir pendaftaran</h1>
@@ -72,13 +72,15 @@
                                     <input type="text" class="form-control" required name="asalkampus"
                                         placeholder="Asal Kampus">
                                 </div>
-                                <div class="form-group">
-                                    <input type="text" class="form-control" required name="reference"
+                                <div class="form-group row">
+                                    <div class="col-sm-6 mb-3 mb-sm-0">
+                                        <input type="text" class="form-control" required name="reference"
                                         placeholder="Referensi">
-                                </div>
-                                <div class="form-group">
-                                    <input type="text" class="form-control" required name="pic"
+                                    </div>
+                                    <div class="col-sm-6 mb-3 mb-sm-0">
+                                        <input type="text" class="form-control" required name="pic"
                                         placeholder="PIC">
+                                    </div>
                                 </div>
                                 <div class="form-group">
                                     <input type="text" class="form-control" required name="angkatan"
