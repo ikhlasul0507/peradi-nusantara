08157a067b453157a698e403a0c4ad9e44ab60f4
Ikhlasul Amal <54785909+ikhlasul0507@users.noreply.github.com>
2024-09-24 20:17:38 +0700

    Update Webhook WA

--- application/config/config.php
+++ application/config/config.php
@@ -472,7 +472,7 @@ $config['csrf_regenerate'] = FALSE;
 $config['csrf_exclude_uris'] = array(
 	'Notification/index',
 	'Notification', // Disable CSRF protection for this URI
-	'Webhook'
+	'Webhook/index'
 );
 
 /*
